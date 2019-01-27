6a325dee599657662613f5e728eb54ec8d7ef674
Adam Bekalarz <adam.bekalarz@allegro.pl>
2019-01-27 22:11:16 +0100

    Prepared for demo with Arqonia 2.0 with HTTP

--- clients-angular/oauth-ui-implicit-angular/src/main/resources/src/app/app.service.ts
+++ clients-angular/oauth-ui-implicit-angular/src/main/resources/src/app/app.service.ts
@@ -19,8 +19,8 @@ export class AppService {
   constructor(
     private _router: Router, private _http: HttpClient, private oauthService: OAuthService){
         this.oauthService.configure({
-            loginUrl: 'https://localhost:8443/oauth/authorize',
-            redirectUri: 'https://localhost:8443/',
+            loginUrl: 'http://localhost:8080/oauth/authorize',
+            redirectUri: 'http://localhost:8083/',
             clientId: 'sampleClientId',
             scope: 'read write foo bar',
             oidc: false
